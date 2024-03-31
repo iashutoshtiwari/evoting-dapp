d3a2f43c1c57651e38cf13d3c79a73146876b0c7
Ashutosh Tiwari <ashutoshtiwari8840@gmail.com>
2024-03-31 15:44:29 +0530

    Initial Commit

---
+++ src/components/finished.jsx
@@ -0,0 +1,9 @@
+const Finished = () => {
+  return (
+    <div className='login-container'>
+      <h1 className='welcome-message'>Voting is Finished</h1>
+    </div>
+  )
+}
+
+export default Finished
---
+++ src/components/login.jsx
@@ -0,0 +1,12 @@
+const Login = (props) => {
+  return (
+    <div className='login-container'>
+      <h1 className='welcome-message'>Welcome to decentralized voting application</h1>
+      <button className='login-button' onClick={props.connectWallet}>
+        Login Metamask
+      </button>
+    </div>
+  )
+}
+
+export default Login
